abe852fd5a4e7cf715d59b39493b0f028a461cce
o-liulchenko <o.liulchenko@ukr.net>
2024-08-30 21:52:46 +0300

    save for blackout

--- src/components/MainSlider/MainSlider.tsx
+++ src/components/MainSlider/MainSlider.tsx
@@ -1,34 +1,37 @@
-import { FC, useState } from 'react';
-import api from '../../api';
+import { FC, useState } from "react";
+import styles from "./MainSlider.module.css";
+import api from "../../api";
 
-interface IMainSliderProps {
-
-}
+interface IMainSliderProps {}
 
 const MainSlider: FC<IMainSliderProps> = () => {
+  let slides = api.getMainPageAnime();
+  let [active, setActive] = useState(0);
 
-  let slides = api.getMainPageAnime()
-  let [active, setActive] = useState(3)
-
-  return <section className='h-[100dvh] overflow-hidden relative'>
-    <ul className='w-full h-full'>
-      {
-        slides.map(({ _id, name, description, image }, index) => (
-          <li key={_id} 
-            className={`absolute inset-0 after:content-[""] after:w-full after:h-full after:absolute after:left-0 after:bottom-0 after:bg-gradient-to-t after:from-white after:from-1% after:to-transparent opacity-0 ${active === index && 'opacity-100'}`}>
-            <img className='w-full h-full object-cover -z-10' src={image} alt={name} />
-            <div className='absolute left-[10%] top-[20%] w-full max-w-[80%] z-20'>
-              <h3>{name}</h3>
+  return (
+    <section className={styles.sliderWrapper}>
+      <ul className={styles.sliderContainer}>
+        {slides.map(({ _id, name, description, image }, index) => (
+          <li key={_id} className={`${styles.slide} ${active === index && styles.active}`}>
+            <img className={styles.slideImage} src={image} alt={name} />
+            <div className={styles.slideDescription}>
+              <h3 className={styles.h3}>{name}</h3>
               <p>{description}</p>
             </div>
           </li>
-        ))
-      }
-    </ul>
-    <div>
-      
-    </div>
-  </section>;
+        ))}
+      </ul>
+      <ul className={styles.sliderControls}>
+        {
+          slides.map(({ _id, name, description, image }, index) => (
+            <li key={_id}>
+              <img src={image} alt={name} />
+            </li>
+          ))
+        }
+      </ul>
+    </section>
+  );
 };
 
 export default MainSlider;
